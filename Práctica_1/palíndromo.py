d95c373f6b052ed7352060615340a6b6a040e30e
Valdi183 <146075137+Valdi183@users.noreply.github.com>
2024-03-12 21:39:29 +0100

    changes: cadena,self.

--- Práctica_1/palíndromo.py
+++ Práctica_1/palíndromo.py
@@ -1,12 +1,12 @@
 """
-Este script, contiene un código para analizar palabras o frases que introduce el usuario, de forma que si son un palíndromo
+Este script, contiene un código para analizar cadenas de texto que introduce el usuario, de forma que si son un palíndromo
 (se leen igual de dercha a izquierda que de izquierda a derecha), muestra True. Mostrará False en caso de que no lo sean
 """
+1
 class Palindromo:
     def __init__(self, cadena):
         self.cadena = cadena
-
-    @classmethod
+            
     def esPalindromo(cls, cadena):
         # Eliminar caracteres no alfanuméricos y convertir a minúsculas
         cadena = ''.join(caracter.lower() for caracter in cadena if caracter.isalnum())
@@ -17,10 +17,15 @@ class Palindromo:
         return self.esPalindromo(self.cadena)
 
     def __del__(self):
-        print(f"El atributo de la instancia en mayúsculas: {self.cadena.upper()}")
+        print(self.cadena.upper())
 
-# Ejemplo de uso
-cadena = input("Palabra a analizar:")
-objeto_palindromo = Palindromo(cadena)
-print("¿Es un palíndromo?", objeto_palindromo.test())
-del objeto_palindromo
+
+p1 = Palindromo("radar")
+print(p1.test())  # True
+
+
+p2 = Palindromo("sonar")
+print(p2.test())  # False
+
+p3 = Palindromo("sonar")
+print(p3.test())  # False
